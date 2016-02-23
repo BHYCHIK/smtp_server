3cf60e72fb1948a12fb788cadcef00315c30959d
Ivan Remen <bhyk@yandex.ru>
2016-02-23 11:34:16 +0300

    Deliveries bugs fixed

--- src/deliver.c
+++ src/deliver.c
@@ -5,9 +5,13 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <errno.h>
 
 #include "session.h"
 #include "deliver.h"
+#include "logger.h"
 
 static int is_local(struct user_session *session, const char *recipient) {
     const char *domain = strrchr(recipient, '@');
@@ -40,7 +44,28 @@ static int any_remote(struct user_session *session) {
     return 0;
 }
 
+static void ensure_mailbox_exists(const char *mailbox) {
+    char log_line[10240];
+    if ( (mkdir(mailbox, 0755) < 0) && (errno != EEXIST) ) {
+        snprintf(log_line, sizeof(log_line), "cannot create %s folder. %s.", mailbox, strerror(errno));
+        send_to_log(log_line);
+    }
+
+    char path[1024];
+    snprintf(path, sizeof(path), "%s/%s", mailbox, "tmp");
+    if ( (mkdir(path, 0755) < 0) && (errno != EEXIST) ) {
+        snprintf(log_line, sizeof(log_line), "cannot create %s folder. %s.", mailbox, strerror(errno));
+        send_to_log(log_line);
+    }
+    snprintf(path, sizeof(path), "%s/%s", mailbox, "new");
+    if ( (mkdir(path, 0755) < 0) && (errno != EEXIST) ) {
+        snprintf(log_line, sizeof(log_line), "cannot create %s folder. %s.", mailbox, strerror(errno));
+        send_to_log(log_line);
+    }
+}
+
 static int get_fd(const char *dirpath, char *filepath, int filepath_size) {
+    ensure_mailbox_exists(dirpath);
     snprintf(filepath, filepath_size, "%s/tmp/mailXXXXXX", dirpath);
     return mkstemp(filepath);
 }
@@ -49,19 +74,21 @@ static int write_file(struct user_session *session, const char *delivered_to, in
     FILE *file = fdopen(fd, "w");
     int size = session->data_size ? session->data_size - 2 : 0;
     fprintf(file, "Return-Path: %s\r\n%s\r\n%.*s", session->from, delivered_to, size, session->data);
-    printf("%s\n", session->data);
     fflush(file);
     fclose(file);
     return 0;
 }
 
 static int make_permament(const char *mailbox, const char *fname) {
+    char log_line[10240];
     char *name = strrchr(fname, '/') + 1;
     char perm_path[1024];
     snprintf(perm_path, sizeof(perm_path), "%s/new/%s", mailbox, name);
     if (rename(fname, perm_path) < 0) {
-        perror("CANNOT RENAME");
+        snprintf(log_line, sizeof(log_line), "Cannot rename file: %s", strerror(errno));
+        return 1;
     }
+    snprintf(log_line, sizeof(log_line), "Delivery done to file %s", perm_path);
     return 0;
 }
 
@@ -73,16 +100,19 @@ static int remote_deliver(struct user_session *session) {
     int tmp_fd = get_fd(remote_mailbox, filepath_tmp, sizeof(filepath_tmp));
 
     char *delivered_to = NULL;
-    asprintf(&delivered_to, "Delivered-To: %s", session->recipients->recipient);
-    struct recipients_list* list = session->recipients->next;
+    asprintf(&delivered_to, "Delivered-To: ");
+    struct recipients_list* list = session->recipients;
     while (list) {
-        char *tmp = NULL;
-        asprintf(&tmp, "%s, %s", delivered_to, list->recipient);
-        free(delivered_to);
-        delivered_to = tmp;
+        if (!is_local(session, list->recipient)) {
+            char *tmp = NULL;
+            asprintf(&tmp, "%s%s, ", delivered_to, list->recipient);
+            free(delivered_to);
+            delivered_to = tmp;
+        }
         list = list->next;
     }
-    write_file(session,delivered_to, tmp_fd);
+    delivered_to[strlen(delivered_to) - 2] = '\0';
+    write_file(session, delivered_to, tmp_fd);
     free(delivered_to);
     make_permament(remote_mailbox, filepath_tmp);
 
@@ -94,13 +124,15 @@ static int local_deliver(struct user_session *session, const char *recipient) {
     if (!config_lookup_string(session->cfg, "LocalMailbox", &local_mailbox)) assert(0);
 
     char filepath_tmp[1024];
-    int tmp_fd = get_fd(local_mailbox, filepath_tmp, sizeof(filepath_tmp));
+    char user_dir[1024];
+    snprintf(user_dir, sizeof(user_dir), "%s/%s", local_mailbox, recipient);
+    int tmp_fd = get_fd(user_dir, filepath_tmp, sizeof(filepath_tmp));
 
     char *delivered_to = NULL;
     asprintf(&delivered_to, "Delivered-To: %s", recipient);
     write_file(session,delivered_to, tmp_fd);
     free(delivered_to);
-    make_permament(local_mailbox, filepath_tmp);
+    make_permament(user_dir, filepath_tmp);
 
     return 0;
 }
